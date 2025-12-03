abedce4194426c57d8087c9aeb3f3d72b783821b
Hans-Mac mini <hanshuang19951204@gmail.com>
2025-12-03 22:34:24 +0800

    feat: 新增 Character、Warrior 和 Wizard 類別以支援角色系統

---
+++ Examer/Examer/Extension/Character.cs
@@ -0,0 +1,60 @@
+namespace Examer.Extension;
+
+public abstract class Character
+{
+    protected Character(string characterType)
+    {
+        throw new NotImplementedException("Please implement the Character() constructor");
+    }
+
+    public abstract int DamagePoints(Character target);
+
+    public virtual bool Vulnerable()
+    {
+        throw new NotImplementedException("Please implement the Character.Vulnerable() method");
+    }
+
+    public override string ToString()
+    {
+        throw new NotImplementedException("Please implement the Character.ToString() method");
+    }
+}
+
+public class Warrior : Character
+{
+    public Warrior() : base("TODO")
+    {
+    }
+
+    public override string ToString()
+    {
+        return "Character is a Warrior";
+    }
+
+    public override int DamagePoints(Character target)
+    {
+        throw new NotImplementedException("Please implement the Warrior.DamagePoints() method");
+    }
+}
+
+public class Wizard : Character
+{
+    public Wizard() : base("TODO")
+    {
+    }
+    
+    public override string ToString()
+    {
+        return "Character is a Wizard";
+    }
+
+    public override int DamagePoints(Character target)
+    {
+        throw new NotImplementedException("Please implement the Wizard.DamagePoints() method");
+    }
+
+    public void PrepareSpell()
+    {
+        throw new NotImplementedException("Please implement the Wizard.PrepareSpell() method");
+    }
+}
