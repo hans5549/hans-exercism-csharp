8bd9a7a42cec5fd50deca327b75a07763bcbcbc2
Hans <hans.huang@gus-tech.com.tw>
2025-12-04 10:37:41 +0800

    feat(player): add player class with random generators
    Implement Player class with randomization methods:
    - RollDie: generate random die roll (1-18)
    - GenerateSpellStrength: generate random spell strength (0-100)

---
+++ Examer/Examer/Extension/Player.cs
@@ -0,0 +1,15 @@
+namespace Examer.Extension
+{
+    public class Player
+    {
+        public int RollDie()
+        {
+            return new Random().Next(1, 19);
+        }
+
+        public double GenerateSpellStrength()
+        {
+            return new Random().NextDouble() * 100;
+        }
+    }
+}
